177ca2de9e6cc59b414d1409de931c7b18def483
Larzsify <larssuffys@hotmail.com>
2017-12-03 11:39:45 +0100

    removed unused dependencies

--- src/components/register-form/register-form.component.ts
+++ src/components/register-form/register-form.component.ts
@@ -2,7 +2,6 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { AuthService } from "../../providers/auth.service";
 import {Account} from "../../models/account/account.interface";
 import { LoginResponse } from "../../models/login/login-response.interface";
-import { NavController } from 'ionic-angular/navigation/nav-controller';
 @Component({
   selector: 'app-register-form',
   templateUrl: 'register-form.component.html'
@@ -12,7 +11,7 @@ export class RegisterFormComponent {
   account = {} as Account
   @Output() registerStatus: EventEmitter<LoginResponse>
 
-  constructor(private auth: AuthService, private navCtrl: NavController) {
+  constructor(private auth: AuthService) {
     this.registerStatus = new EventEmitter<LoginResponse>();
   }
   async register(){
